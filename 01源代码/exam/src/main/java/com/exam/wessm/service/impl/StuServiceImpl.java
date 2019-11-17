1c2ba0aa081fa94ecb397295e699e39150cf76dc
123 <vip@vip.com>
2019-11-17 14:08:58 +0800

    stuDao,service,mapper

--- 01源代码/exam/src/main/java/com/exam/wessm/service/impl/StuServiceImpl.java
+++ 01源代码/exam/src/main/java/com/exam/wessm/service/impl/StuServiceImpl.java
@@ -1,4 +1,45 @@
 package com.exam.wessm.service.impl;
 
-public class StuServiceImpl {
+import com.exam.wessm.dao.IStuDao;
+import com.exam.wessm.entity.Stu;
+import com.exam.wessm.service.IStuService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service("stuService")
+public class StuServiceImpl implements IStuService {
+    @Autowired
+    @Qualifier("stuDao")
+    private IStuDao stuDao;
+    @Override
+    public int registerStu(Stu stu) {
+        return stuDao.registerStu(stu);
+    }
+
+    @Override
+    public Stu findNoAndPass(String sNo, String sPassword) {
+        return stuDao.findNoAndPass(sNo,sPassword);
+    }
+
+    @Override
+    public int deleteStu(int sId) {
+        return stuDao.deleteStu(sId);
+    }
+
+    @Override
+    public List<Stu> queryStu() {
+        return stuDao.queryStu();
+    }
+
+    @Override
+    public Stu getStu(int sId) {
+        return stuDao.getStu(sId);
+    }
+
+    @Override
+    public int updateStu(Stu stu) {
+        return stuDao.updateStu(stu);
+    }
 }
